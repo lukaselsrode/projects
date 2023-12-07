b7bc0bdeff92a822ab7d948139f1824a4789bd4c
wolfy <wolfy@greenman.myfiosgateway.com>
2023-12-07 13:17:27 -0500

    using yes command to make it fully auto

--- sys_admin/sys_update.sh
+++ sys_admin/sys_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'os.info' && cat /etc/os*;
-sudo apt dist-upgrade && sudo apt update && sudo apt full-upgrade -y;
-sudo apt-get dist-upgrade && sudo apt-get update && sudo apt-get upgrade;
-sudo apt-get clean && sudo apt-get autoremove;
+yes | sudo apt dist-upgrade && sudo apt update && sudo apt full-upgrade -y;
+yes | sudo apt-get dist-upgrade && sudo apt-get update && sudo apt-get upgrade;
+yes | sudo apt-get clean && sudo apt-get autoremove;
 update-grub
