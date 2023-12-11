0efba1741a6d69fe83c92a46d3516407a2556e7a
wolfy <wolfy@greenman.myfiosgateway.com>
2023-12-11 12:45:05 -0500

    didnt know grub had cmd prompts

--- sys_admin/sys_update.sh
+++ sys_admin/sys_update.sh
@@ -3,4 +3,4 @@ echo 'os.info' && cat /etc/os*;
 yes | sudo apt dist-upgrade && sudo apt update && sudo apt full-upgrade -y;
 yes | sudo apt-get dist-upgrade && sudo apt-get update && sudo apt-get upgrade;
 yes | sudo apt-get clean && sudo apt-get autoremove;
-update-grub
+yes | update-grub
